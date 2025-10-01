1fce697f4ac3fed233c0d08ff8b994a534da48a7
Pratap Singh Mehra <pratap@mehra.com>
2025-10-01 02:40:29 -0400

    added doc

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/peek-a-boo/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
