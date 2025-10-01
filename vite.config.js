9963339d7fb06ab34164f900bef9d976f877ca85
Pratap Singh Mehra <pratap@mehra.com>
2025-10-01 13:48:54 -0400

    npx serve docs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/peek-a-boo/" : "/",
+  base: "/peek-a-boo/",
   build: {
     outDir: "docs",
     emptyOutDir: true,
